d4875956979bd15cbec31c372c4255fd21457494
Sergey Grebenshchikov <sgreben@gmail.com>
2018-05-17 20:11:06 +0200

    Updated

--- docker-debug/step1_demo.sh
+++ docker-debug/step1_demo.sh
@@ -1,6 +1,4 @@
-#!/bin/sh +v
-pwd
-script=./build_and_run.sh
-until [ -f "$script" ]; do sleep 1; done
-chmod +x "$script"
-"$script"
+SCRIPT=./build_and_run.sh
+while [ ! -f "$SCRIPT" ]; do sleep 1; done
+chmod +x "$SCRIPT"
+"$SCRIPT"
