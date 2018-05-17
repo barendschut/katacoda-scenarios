34215bcd5ffdffe0c64dcb07951bc65a8269fb5a
Sergey Grebenshchikov <sgreben@gmail.com>
2018-05-17 19:42:51 +0200

    Updated

--- docker-debug/intro_setup.sh
+++ docker-debug/intro_setup.sh
@@ -1,4 +1,4 @@
 HOME=/home/scrapbook/tutorial
 mkdir -p "$HOME"/nginx-config/
-cp "$HOME"/nginx.conf "$HOME"/nginx-config/nginx.conf
+#cp "$HOME"/nginx.conf "$HOME"/nginx-config/nginx.conf
 docker run -d -p 8080:80 --mount type=bind,source="$HOME"/nginx-config/nginx.conf,target=/etc/nginx/nginx.conf nginx:1.13-alpine
