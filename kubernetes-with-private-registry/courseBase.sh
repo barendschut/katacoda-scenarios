2bffbed6791da9876aaba51569aa8ce6075dc009
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-02-15 23:34:16 +0100

    Updated

--- kubernetes-with-private-registry/courseBase.sh
+++ kubernetes-with-private-registry/courseBase.sh
@@ -1,1 +1,1 @@
-ssh root@master "curl -sSL https://gist.githubusercontent.com/sgreben/ad6a970948642f251a9ecfc9366f618f/raw | sed 's/HOST_IP/[[HOST_IP]]/g;s/HOST2_IP/[[HOST2_IP]]/g;s/HOST1/[[HOST1]]/g;s/HOST2/[[HOST2]]/g;s/HOST/[[HOST]]/g;' > /opt/setup.sh; chmod +x /opt/setup.sh; /opt/setup.sh"
+ssh root@master "curl -sSL https://gist.githubusercontent.com/sgreben/ad6a970948642f251a9ecfc9366f618f/raw | sed 's/HOST_IP/[[HOST_IP]]/g;s/HOST2_IP/[[HOST2_IP]]/g;s/HOST1/[[HOST1]]/g;s/HOST2/[[HOST2]]/g;s/HOST1/[[HOST]]/g;' > /opt/setup.sh; chmod +x /opt/setup.sh; /opt/setup.sh"
