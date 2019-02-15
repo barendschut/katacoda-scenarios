27b65ff3400455dd9230536684ae0fb083f07026
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-02-15 21:59:12 +0100

    Updated

--- kubernetes-with-private-registry/intro_setup.sh
+++ kubernetes-with-private-registry/intro_setup.sh
@@ -1,4 +1,3 @@
-set -eu;
 alias ssh="ssh -oBatchMode=yes -o TCPKeepAlive=yes -o ServerAliveInterval=30 -o ServerAliveCountMax=30 -o ConnectTimeout=30 -o ConnectionAttempts=30 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=error";
 alias scp="scp -oBatchMode=yes -o TCPKeepAlive=yes -o ServerAliveInterval=30 -o ServerAliveCountMax=30 -o ConnectTimeout=30 -o ConnectionAttempts=30 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=error";
 REGISTRY_DOMAIN=registry.workshop.breda.local;
