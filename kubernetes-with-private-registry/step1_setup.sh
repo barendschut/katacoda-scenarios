fecc563a61263e6da340c4e892e7b4af768b14fc
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-02-16 01:13:20 +0100

    Updated

--- kubernetes-with-private-registry/step1_setup.sh
+++ kubernetes-with-private-registry/step1_setup.sh
@@ -4,33 +4,33 @@ set +x
 REGISTRY_DOMAIN=registry.workshop.breda.local;
 
 waitForDockerRegistryLocal() {
-    echo "Waiting for Docker registry..."
+    echo "[$(date)] Waiting for Docker registry..."
     until
         >/dev/null 2>/dev/null docker inspect -f '{{.ID}}' registry;
     do
         sleep 1
     done
-    echo "done"
+    echo "[$(date)] done"
 }
 
 waitForDockerRegistryRemote() {
-    echo "Waiting for Docker registry..."
+    echo "[$(date)] Waiting for Docker registry..."
     until
         >/dev/null 2>/dev/null curl -sSL https://"$REGISTRY_DOMAIN":5000/v2/
     do
         sleep 1
     done
-    echo "done"
+    echo "[$(date)] done"
 }
 
 waitForKubernetes() {
-    echo "Waiting for Kubernetes..."
+    echo "[$(date)] Waiting for Kubernetes..."
     until
         >/dev/null 2>/dev/null kubectl version;
     do
         sleep 1
     done
-    echo "done"
+    echo "[$(date)] done"
 }
 
 case "$(hostname)" in
