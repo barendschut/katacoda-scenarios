9ca41662f90a30be3d9d360b25baa3eb6699e10d
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-02-16 23:34:22 +0100

    Updated

--- kubernetes-with-private-registry/step1_setup.sh
+++ kubernetes-with-private-registry/step1_setup.sh
@@ -3,108 +3,110 @@
 set +x
 REGISTRY_DOMAIN=registry.workshop.breda.local;
 
+alias simple_date="date +'%H:%M:%S'"
+
 waitForDockerRegistryLocal() {
-    echo "[$(date)] Waiting for Docker registry... (<1 min)"
+    echo "[$(simple_date)] Waiting for Docker registry... (<1 min)"
     until
         >/dev/null 2>/dev/null docker inspect -f '{{.ID}}' registry;
     do
         sleep 1
         echo .;
     done | stdin-spinner;
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 waitForNetwork() {
-    echo "[$(date)] Waiting for network connectivity... (<1 min)"
+    echo "[$(simple_date)] Waiting for network connectivity... (<1 min)"
     until
         >/dev/null 2>/dev/null curl  --fail --connect-timeout 1 --head https://github.com/
     do
         sleep 1
         echo .;
     done | stdin-spinner;
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 waitForDockerRegistryRemote() {
-    echo "[$(date)] Waiting for Docker registry... (<1 min)"
+    echo "[$(simple_date)] Waiting for Docker registry... (<1 min)"
     until
         2>&1 curl -sSL https://"$REGISTRY_DOMAIN"/v2/
     do
         sleep 1
     done | stdin-spinner;
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 waitForKubernetes() {
-    echo "[$(date)] Waiting for Kubernetes... (~5 sec)"
+    echo "[$(simple_date)] Waiting for Kubernetes... (~5 sec)"
     until
         2>&1 kubectl version;
     do
         sleep 1;
     done | stdin-spinner;
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 waitForWeave() {
-    echo "[$(date)] Waiting for Weave... (~5 sec)"
+    echo "[$(simple_date)] Waiting for Weave... (~5 sec)"
     2>&1 kubectl wait daemonset/weave-net -n kube-system --for condition=available | stdin-spinner;
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 killKubeProxyPods() {
-    echo "[$(date)] Restarting kube-proxy... (~2 sec)"
+    echo "[$(simple_date)] Restarting kube-proxy... (~2 sec)"
     (
         2>&1 kubectl delete pods -lk8s-app=kube-proxy -n kube-system;
         2>&1 kubectl wait daemonset/kube-proxy -n kube-system --for condition=available;
     ) | stdin-spinner
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 killCoreDNSPods() {
-    echo "[$(date)] Restarting core-dns... (~2 sec)"
+    echo "[$(simple_date)] Restarting core-dns... (~2 sec)"
     (
         2>&1 kubectl delete pods -lk8s-app=coredns -n kube-system;
         2>&1 kubectl wait deployments/coredns -n kube-system --for condition=available;
     ) | stdin-spinner
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 deployMetricsServer() {
-    echo "[$(date)] Deploying metrics-server... (~3 sec)"
+    echo "[$(simple_date)] Deploying metrics-server... (~3 sec)"
     (
         2>&1 git clone --single-branch --depth=1 https://github.com/kubernetes-incubator/metrics-server
         2>&1 kubectl create -f metrics-server/deploy/1.8+/
     ) | stdin-spinner
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 deployDashboard() {
-    echo "[$(date)] Deploying Kubernetes dashboard... (~3 sec)"
+    echo "[$(simple_date)] Deploying Kubernetes dashboard... (~3 sec)"
     (
         2>&1 kubectl apply -f https://gist.github.com/sgreben/bd04d51eb2f683091ba62d7389a564a8/raw/be1065bdda39ae4948e701e2ea2ba9975a6ccffa/dashboard.yaml
         2>&1 kubectl wait deployments/kubernetes-dashboard -n kube-system --for condition=available;
     ) | stdin-spinner
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 installKubebox() {
-    echo "[$(date)] Installing kubebox... (~3 sec)"
+    echo "[$(simple_date)] Installing kubebox... (~3 sec)"
     (
         2>&1 curl -Lo kubebox https://github.com/astefanutti/kubebox/releases/download/v0.4.0/kubebox-linux
     ) | stdin-spinner
     chmod +x kubebox
     mv kubebox /usr/bin/
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 installKail() {
-    echo "[$(date)] Installing kail... (~3 sec)"
+    echo "[$(simple_date)] Installing kail... (~3 sec)"
     (
         curl -sSL https://github.com/boz/kail/releases/download/v0.7.0/kail_0.7.0_linux_amd64.tar.gz | tar xvz 2>&1
     ) | stdin-spinner
     chmod +x kail
     mv kail /usr/bin/
-    echo "[$(date)] done"
+    echo "[$(simple_date)] done"
 }
 
 installStdinSpinner() {
@@ -117,7 +119,7 @@ installSSHKey() {
     >/dev/null 2>/dev/null chmod 400 ~/.ssh/k8s_workshop_breda;
     >/dev/null 2>/dev/null eval "$(ssh-agent)"
     >/dev/null 2>/dev/null ssh-add ~/.ssh/k8s_workshop_breda;
-    echo "[$(date)] SSH public key:
+    echo "[$(simple_date)] SSH public key:
 
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7HCf/bOWHHV73rYHrP89vnPQJNkHitUo72jwuVyYg6/LeNWG4KwIhzs9BRHNqcZp90NjfbibKCchmVZnuylXkyE3YwfYCAt1lZ6zWBt2jcPGRCBDfaqlZEAXjgjOywMM1KMzf9SZAJBQTYsC893BImclg6wfORm/RZupakP7QYixPNjo94W9HGkMeO6fYdI2uk48/T+qKw0kdFdw3DTRXaxSFmof+4NdSxk8N5Hf9W2l2AWNkOZlRnhQgnwI++thfwbAhu4OjY17P8Fdazc+NhYO+OuOUMdBzVDs+88kD5jq5mS/NxUSK+ShywIpqlTnk98RyFTNoM3nnWGIX5uzh k8s-workshop@breda
 
