89c14f7b489e0bc407a1c550915041e92bf09f61
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-02-16 01:48:01 +0100

    Updated

--- kubernetes-with-private-registry/step1_setup.sh
+++ kubernetes-with-private-registry/step1_setup.sh
@@ -39,8 +39,10 @@ installKubebox() {
     echo "[$(date)] done"
 }
 
-printSSHPublicKey() {
-    chmod 400 ~/.ssh/k8s_workshop_*;
+installSSHKey() {
+    chmod 400 ~/.ssh/k8s_workshop_breda;
+    eval "$(ssh-agent)"
+    ssh-add ~/.ssh/k8s_workshop_breda;
     echo "[$(date)] SSH public key:
 
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7HCf/bOWHHV73rYHrP89vnPQJNkHitUo72jwuVyYg6/LeNWG4KwIhzs9BRHNqcZp90NjfbibKCchmVZnuylXkyE3YwfYCAt1lZ6zWBt2jcPGRCBDfaqlZEAXjgjOywMM1KMzf9SZAJBQTYsC893BImclg6wfORm/RZupakP7QYixPNjo94W9HGkMeO6fYdI2uk48/T+qKw0kdFdw3DTRXaxSFmof+4NdSxk8N5Hf9W2l2AWNkOZlRnhQgnwI++thfwbAhu4OjY17P8Fdazc+NhYO+OuOUMdBzVDs+88kD5jq5mS/NxUSK+ShywIpqlTnk98RyFTNoM3nnWGIX5uzh k8s-workshop@breda
@@ -60,6 +62,6 @@ case "$(hostname)" in
         clear
         waitForDockerRegistryRemote
         waitForKubernetes
-        printSSHPublicKey
+        installSSHKey
     ;;
 esac
