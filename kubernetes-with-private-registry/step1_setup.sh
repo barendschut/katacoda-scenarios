7d2b2fe488ef2dc0c074d8f5b1b80e137f110399
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-02-17 22:04:14 +0100

    Updated

--- kubernetes-with-private-registry/step1_setup.sh
+++ kubernetes-with-private-registry/step1_setup.sh
@@ -18,10 +18,12 @@ waitForDockerRegistryLocal() {
 
 waitForDockerUpgrade() {
     echo "[$(simple_date)] Upgrading docker... (<1 min)"
-    until [ -e /opt/upgrade-docker ] || [ -e /opt/upgrade-docker-done ]; do
-        sleep 1;
-    done;
-    ( 2>&1 cat /opt/upgrade-docker || true) | stdin-spinner;
+    (
+        until [ -e /opt/upgrade-docker ] || [ -e /opt/upgrade-docker-done ]; do
+            sleep 1;
+        done;
+        2>&1 cat /opt/upgrade-docker || true;
+    ) | stdin-spinner;
     echo "[$(simple_date)] done"
 }
 
