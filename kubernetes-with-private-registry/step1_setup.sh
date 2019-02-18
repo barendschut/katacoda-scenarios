8b9de48694c6e52816a15b1f60efbce1fb882403
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2019-02-18 02:33:32 +0100

    Updated

--- kubernetes-with-private-registry/step1_setup.sh
+++ kubernetes-with-private-registry/step1_setup.sh
@@ -165,10 +165,10 @@ case "$(hostname)" in
             killKubeDNSPods
             waitForDockerRegistryRemote
             waitForKubernetes
+            killKubeProxyPods
             deployDashboard
-            # waitForWeave
-            # killKubeProxyPods
-            # killCoreDNSPods
+            waitForWeave
+            killCoreDNSPods
         ) | stdin-spinner
         echo "[$(simple_date)] done"
         printf "\033[?25h"
